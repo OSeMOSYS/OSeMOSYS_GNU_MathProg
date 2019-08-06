3711e6c0b0c4209bb42c4a8cce2820af2def3e42
Will Usher <wusher@kth.se>
2019-08-06 10:09:59 +0200

    Fixes running tests from any location
    - Added utility function to test suite which fixes paths. This allows
    the tests to be run from a parent folder and the fixtures not to break
    - Added a short readme to the tests directory

--- tests/test_gnu_mathprog.py
+++ tests/test_gnu_mathprog.py
@@ -1,13 +1,20 @@
+import os
 import pytest
 
 from subprocess import run
+
+def get_folder():
+    return os.path.dirname(os.path.abspath(__file__))
 
 class Test_RunMathProg():
 
     def test_mathprog_run_normal(self):
 
-        arguments = ['glpsol', '-m', 'src/osemosys.txt', '-d', 
-                     'tests/utopia.txt', '-o', 'results.csv']
+        model_file = os.path.join(get_folder(), '../src/osemosys.txt')
+        data_file = os.path.join(get_folder(), 'utopia.txt')
+
+        arguments = ['glpsol', '-m', model_file, '-d', 
+                     data_file, '-o', './results.csv']
         output = run(arguments, capture_output=True, text=True)
         assert 'OPTIMAL LP SOLUTION FOUND' in output.stdout
         assert 'obj =   2.944686269e+04' in output.stdout
@@ -15,8 +22,11 @@ class Test_RunMathProg():
 
     def test_mathprog_run_short(self):
 
-        arguments = ['glpsol', '-m', 'src/osemosys_short.txt', '-d', 
-                     'tests/utopia.txt', '-o', 'results_short.csv']
+        model_file = os.path.join(get_folder(), '../src/osemosys_short.txt')
+        data_file = os.path.join(get_folder(), 'utopia.txt')
+
+        arguments = ['glpsol', '-m', model_file, '-d', 
+                     data_file, '-o', './results_short.csv']
         output = run(arguments, capture_output=True, text=True)
         assert 'OPTIMAL LP SOLUTION FOUND' in output.stdout
         assert 'obj =   2.944686269e+04' in output.stdout
