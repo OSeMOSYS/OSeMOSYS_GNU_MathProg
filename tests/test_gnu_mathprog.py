b47f19bd7e782e122baaf38180d0b265e7611bb1
Will Usher <wusher@kth.se>
2020-03-10 11:02:59 +0100

    Updated tests to work with CSV files

--- tests/test_gnu_mathprog.py
+++ tests/test_gnu_mathprog.py
@@ -1,5 +1,6 @@
 import os
 from pytest import fixture
+import pandas as pd
 
 from subprocess import run
 
@@ -18,11 +19,11 @@ def short_model_file():
 def data_file():
     return os.path.join(get_folder(), 'utopia.txt')
 
-@fixture(scope="session")
+@fixture(scope='session')
 def results_folder(tmp_path_factory):
-    path = tmp_path_factory.mktemp("results")
-    fn = os.path.join(str(path),'results.csv')
-    return fn
+    path = tmp_path_factory.mktemp("test")
+    os.mkdir(os.path.join(str(path), 'results'))
+    return os.path.join(str(path))
 
 @fixture(scope="session")
 def lp_folder(tmp_path_factory):
@@ -80,12 +81,13 @@ class TestNormalWithCBC():
 
 class TestNormal():
 
-    @fixture(scope='function')
+    @fixture(scope='session')
     def run_model(self, model_file, data_file, results_folder):
         arguments = ['glpsol', '-m', model_file, '-d',
-                     data_file, '-o', results_folder]
+                     data_file]
+        os.chdir(os.path.join(results_folder))
         output = run(arguments, capture_output=True, text=True)
-        return output, results_folder
+        return output, os.path.join(results_folder, 'results')
 
     def test_mathprog_run_normal(self, run_model):
 
@@ -95,14 +97,56 @@ class TestNormal():
 
     def test_results_exist(self, run_model):
         _, results_folder = run_model
-        assert os.path.exists(results_folder)
+        assert os.path.exists(str(results_folder))
 
-    def test_results_read(self, run_model):
+    def test_results_read_accumulated_new_capacity(self, run_model):
+        """
+        """
+        _, results_folder = run_model
+        result_file = os.path.join(str(results_folder), 'AccumulatedNewCapacity.csv')
+        actual = pd.read_csv(result_file)
+        actual = actual[actual.YEAR == 2010].reset_index(drop=True)
+
+        expected = pd.DataFrame(columns=['REGION','TECHNOLOGY','YEAR','VALUE'],
+                                data=[['UTOPIA',        'E01',  2010,    2.279801],
+                                      ['UTOPIA',        'E31',  2010,    0.110000],
+                                      ['UTOPIA',    'IMPDSL1',  2010,   77.597496],
+                                      ['UTOPIA',    'IMPHCO1',  2010,  191.565506],
+                                      ['UTOPIA',        'RHE',  2010,   46.867723],
+                                      ['UTOPIA',        'RHO',  2010,   46.135248],
+                                      ['UTOPIA',        'RL1',  2010,   18.901890],
+                                      ['UTOPIA',        'SRE',  2010,    0.100000],
+                                      ['UTOPIA',        'TXD',  2010,   11.690000],
+                                      ['UTOPIA',        'RIV',  2010,    5.587785]])
+
+        pd.testing.assert_frame_equal(actual, expected)
+
+
+    def test_results_read_new_capacity(self, run_model):
+        """
+        """
         _, results_folder = run_model
 
-        actual = check_results(results_folder, 'CAa1_TotalNewCapacity[UTOPIA,E01,1990]')
-        assert actual['Activity'] == '0'
-        assert actual['Marginal'] == '39.036'
+        result_file = os.path.join(str(results_folder), 'NewCapacity.csv')
+        actual = pd.read_csv(result_file)
+        actual = actual.groupby(by=['REGION', 'TECHNOLOGY'],
+                                as_index=False).sum().drop(columns='YEAR')
+
+        expected = pd.DataFrame(columns=['REGION','TECHNOLOGY','VALUE'],
+                                data=[
+                                    ['UTOPIA',        'E01',     2.279801],
+                                    ['UTOPIA',        'E31',     0.110000],
+                                    ['UTOPIA',    'IMPDSL1',  1717.805326],
+                                    ['UTOPIA',    'IMPHCO1',  1451.847478],
+                                    ['UTOPIA',        'RHE',    46.867723],
+                                    ['UTOPIA',        'RHO',    46.135248],
+                                    ['UTOPIA',        'RIV',    97.919280],
+                                    ['UTOPIA',        'RL1',    34.303990],
+                                    ['UTOPIA',        'SRE',     0.100000],
+                                    ['UTOPIA',        'TXD',    17.790000],
+                                      ])
+
+        pd.testing.assert_frame_equal(actual, expected)
 
 
 class TestShort():
